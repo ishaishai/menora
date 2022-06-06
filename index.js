42d015973d160cb46b7e161e570c3152ab14fa89
Ishai Yaish <ishaiyaish@gmail.com>
2022-06-06 19:12:27 +0300

    added path

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require('path');
 const apiRoutes = require("./routes/api.routes");
 
 const PORT = process.env.PORT || 5000;
